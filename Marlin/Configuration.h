7a29dd50744024ca286c32573dc6377cf69605c5
Kateenkorva <86017964+Kateenkorva@users.noreply.github.com>
2022-09-03 23:09:41 +0300

    Asetukset päivitetty, dual z drivers ja liikkuu

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1167,7 +1167,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 412.82 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.41, 80.35, 401.95, 412.82 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
